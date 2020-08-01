cb9cfe97418a058e08f4f9b4e5dd4ce556676c52
Nancy Jackson <nbjackson23@gmail.com>
2020-08-01 18:31:03 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ if sentence == "Hi Nana, how are you?" # Whatever you say to grandma, she  shoul
   return  "HUH?! SPEAK UP, SONNY!"
 # unless you shout it (type in all capitals).
 elsif sentence== "Hi!"
-  return "HUH?! SPEAK UP SONNY!" # If you shout, she can hear you (or at least she thinks so) 
+  return "HUH?! SPEAK UP, SONNY!" # If you shout, she can hear you (or at least she thinks so) 
 elsif sentence== "WHAT DID YOU EAT TODAY?" # and yells back
   return "NO, NOT SINCE 1938!"
 elsif sentence== "WHAT?"# NO, NOT SINCE 1938!
